adb8ed6f2364244e712a10a5dc2f9fd93e704918
ololoshka-cpu <58772044+ololoshka-cpu@users.noreply.github.com>
2019-12-11 14:35:51 +0300

    Create lab1-1.c

---
+++ lab1-1.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+using namespace std;
+
+int main(int argc, char* argv[]) {
+	int k = 1;
+	char error[] = "No multiples found";
+	char error1[] = "Too many arguments";
+	if (argc != 2) {
+		printf("%s/n", error1);
+		return 0;
+	}
+	if (atoi(argv[1]) == 0) {
+		printf("%s\n", error);
+		return 0;
+	}
+	while (k * atoi(argv[1]) <= 100) {
+		printf("%d\n", k * atoi(argv[1]));
+		k++;
+	}
+	if (k == 1)
+		printf("%s\n", error);
+
+	return 0;
+}
