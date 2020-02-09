9694179d980fd3daabc81bc20337d6afa720f291
ololoshka-cpu <58772044+ololoshka-cpu@users.noreply.github.com>
2020-02-09 07:29:53 +0300

    Update lab1-08.c

--- lab1-08.c
+++ lab1-08.c
@@ -2,10 +2,16 @@
 
 int main() {
 	float a, b, c;
+	bool answer;
 	scanf("%f %f %f", &a, &b, &c);
-	if (a + b <= c || b + c <= a || c + a <= b)
+	if (a >= b && a >= c)
+		answer = a * a == b * b + c * c;
+	else if (b >= a && b >= c)
+		answer = b * b == a * a + c * c;
+	else
+		answer = c * c == a * a + b * b;
+	if (!answer)
 		printf("These values cannot be the lengths of the sides of the triangle");
 	else
 		printf("These values can be the lengths of the sides of the triangle");
 	return 0;
-}
